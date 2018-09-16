25896b13252a38ad62c7bfd2ef34143b3ac54162
nobii <fujisawa-shin@kayac.com>
2018-09-16 20:12:45 +0900

    調節

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const octokit = require('@octokit/rest')();
-const argv = require('minimist')(process.argv.slice(2));
+const octokit = require("@octokit/rest")();
+const argv = require("minimist")(process.argv.slice(2));
 
-octokit.authenticate(require('./auth.json'));
+octokit.authenticate(require("./auth.json"));
 
-const [ target ] = argv._;
-const [owner,repo] = target.split(/\//);
+const [target] = argv._;
+const [owner, repo] = target.split(/\//);
 
-const opts = {
+const baseOpts = {
   owner,
-  repo,
-  title: 'test',
-  body: ['テスト本文', '二行以上の文章'].join('\n'),
-  // assignee: 'fnobi',
-  labels: ['design'],
+  repo
 };
+
+const opts = Object.assign({}, baseOpts, {
+  title: "test",
+  body: ["テスト本文", "二行以上の文章"].join("\n"),
+  // assignee: 'fnobi',
+  labels: ["design"]
+});
 
 console.log(opts);
 
