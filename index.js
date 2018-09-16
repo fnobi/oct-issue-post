d6159e9c06310b1d19838729202f6329c5962dd5
nobii <fujisawa-shin@kayac.com>
2018-09-17 00:42:15 +0900

    分離の仕方を考え直した

--- index.js
+++ index.js
@@ -3,25 +3,17 @@ const octokit = require("@octokit/rest")();
 const parse = require("csv-parse");
 const fs = require("mz/fs");
 const _ = require("lodash");
-const argv = require("minimist")(process.argv.slice(2));
 require("colors");
 
-const reducer = require("./reducer");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-const [target, csv] = argv._;
-const [owner, repo] = target.split(/\//);
-
-const baseOpts = {
-  owner,
-  repo
+const MSG = ["Do you create issue with this option?".green, "[Y|n]"].join(" ");
+const DEFAULT_REDUCER = ([ title, body, assignee, labels ]) => {
+  return {
+    title,
+    body,
+    assignee: assignee.split(/,/g),
+    labels: labels.split(/,/g),
+  }
 };
-
-octokit.authenticate(require("./auth.json"));
 
 function loadCsv(filePath) {
   return fs.readFile(filePath, "utf8").then(body => {
@@ -37,17 +29,13 @@ function loadCsv(filePath) {
   });
 }
 
-function postIssue(row) {
+function postIssue({ row, rl, baseOpts, reducer = DEFAULT_REDUCER }) {
   return new Promise((resolve, reject) => {
     const issueOpts = reducer(row);
     const opts = Object.assign({}, baseOpts, issueOpts);
     console.log(opts);
 
-    const msg = ["Do you create issue with this option?".green, "[Y|n]"].join(
-      " "
-    );
-
-    rl.question(msg, answer => {
+    rl.question(MSG, answer => {
       if (answer === "Y") {
         octokit.issues.create(opts).then(resolve, reject);
       } else {
@@ -57,14 +45,30 @@ function postIssue(row) {
   });
 }
 
-loadCsv(csv).then(rows => {
-  let p = Promise.resolve();
-  _.each(rows, row => {
-    p = p.then(() => {
-      return postIssue(row);
+module.exports = function({ owner, repo, csv, reducer, auth }) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  const baseOpts = {
+    owner,
+    repo
+  };
+
+  octokit.authenticate(auth);
+
+  return Promise.resolve()
+    .then(() => loadCsv(csv))
+    .then(rows => {
+      let p = Promise.resolve();
+      _.each(rows, row => {
+        p = p.then(() => {
+          return postIssue({ row, rl, baseOpts, reducer });
+        });
+      });
+      return p.then(() => {
+        rl.close();
+      });
     });
-  });
-  p.then(() => {
-    rl.close();
-  });
-});
+};
